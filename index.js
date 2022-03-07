f4a4aff87b6aa7466de67104fb654100a1f13418
Rodrigo <rodrigo.prestes@gmail.com>
2022-03-07 20:12:53 -0300

    tests

---
+++ index.js
@@ -0,0 +1,74 @@
+
+/**
+ * Recebe um array com os nomes de pessoas e retorna um array invertido
+ *
+ * @param {*} people : Um array contendo um conjunto de nomes
+ * @returns Um array com os nomes invertidos
+ */
+function invert(people){
+
+    // TODO
+    // 1) Peça ao usuário para digitar vários nomes. Exiba na tela
+    // todos os nomes digitados, porém de maneira invertida (do último para o primeiro).
+    // Uma dica, você pode utilizar a função prompt para permitir que o usuário digite os
+    // nomes dos usuários.
+
+    return [];
+}
+
+/**
+ * Essa função recebe um array de notas e retorna uma média
+ *
+ * @param {*} grades : Um array de notas
+ * @returns Uma média a partir do array de notas
+ */
+function mean(grades){
+
+    // TODO
+    // 2) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
+    // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
+    // ou superior a 7.
+
+    return 0;
+}
+
+/**
+ * Função recebe uma média e informa textualmente se um estudante está aprovado ou não
+ *
+ * @param {*} mean : Um número que representa uma média
+ * @returns Uma string dizendo se um estudante está aprovado ou não
+ */
+function isApproved(mean){
+
+    // TODO
+    // 2.1) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
+    // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
+    // ou superior a 7.
+
+    return "";
+}
+
+/**
+ * Recebe uma data no formato de string e transforma em um formato textual
+ *
+ * @param {*} strDate : Uma string no formato de data
+ * @returns Uma descrição da data informada
+ */
+function wide(strDate){
+
+    // TODO
+    // 3) Faça um script que receba uma data no formato “dd/mm/aaaa” e escreva a data por extenso,
+    // por exemplo, de "03/03/2022" para "03 de março de 2022". Dica: use a função
+    // “split” de uma string que quebra a string em pedaços dado um separador como argumento da função.
+    // Nesse caso, o separador é a barra (/) da data.
+
+    return "";
+}
+
+// Trecho utilizado pelos testes
+exports._test = {
+    invert: invert,
+    mean: mean,
+    isApproved: isApproved,
+    wide: wide
+}
---
+++ test/tests.js
@@ -0,0 +1,105 @@
+var index = require('../index.js');
+var assert = require('assert');
+
+describe('Correção', function () {
+
+  // Issue 1
+
+  describe('invert', function () {
+    it('Invert com cinco elementos', function () {
+      var people = ['Dora', 'Ari', 'Maria', 'Rodrigo', 'Cristiane'];
+        assert.deepEqual(index._test.invert(people), ['Cristiane', 'Rodrigo', 'Maria', 'Ari', 'Dora']);
+    });
+  });
+
+  describe('invert', function () {
+    it('Invert com um elemento', function () {
+      var people = ['Rodrigo'];
+        assert.deepEqual(index._test.invert(people), ['Rodrigo']);
+    });
+  });
+
+  describe('invert', function () {
+    it('Invert com dois elementos iguais', function () {
+      var people = ['Rodrigo', 'Rodrigo'];
+        assert.deepEqual(index._test.invert(people), ['Rodrigo', 'Rodrigo']);
+    });
+  });
+
+  describe('invert', function () {
+    it('Invert com um array vazio', function () {
+        var people = [];
+        assert.deepEqual(index._test.invert(people), []);
+    });
+  });
+
+
+  // Issue 2
+
+  describe('mean', function () {
+    it('Mean com três valores', function () {
+        var grade = [6,7,10];
+        assert.deepEqual(index._test.mean(grade), 7.666666666666667);
+    });
+  });
+
+  describe('isApproved', function () {
+    it('isApproved: 10', function () {
+        var grade = 10;
+        assert.deepEqual(index._test.isApproved(grade), "aprovado");
+    });
+  });
+
+  describe('isApproved', function () {
+    it('isApproved: 7', function () {
+        var mean = 7;
+        assert.deepEqual(index._test.isApproved(mean), "aprovado");
+    });
+  });
+
+  describe('isApproved', function () {
+    it('isApproved: 5', function () {
+        var mean = 5;
+        assert.deepEqual(index._test.isApproved(mean), "reprovado");
+    });
+  });
+
+
+  // Issue 3
+
+  describe('wide', function () {
+    it('Wide: 03/03/22', function () {
+        var strDate = "03/03/22";
+        assert.deepEqual(index._test.wide(strDate), "03 de março de 22");
+    });
+  });
+
+  describe('wide', function () {
+    it('Wide: 10/06/98', function () {
+        var strDate = "10/06/98";
+        assert.deepEqual(index._test.wide(strDate), "10 de outubro de 98");
+    });
+  });
+
+  describe('wide', function () {
+    it('Wide: 25/08/1994', function () {
+        var strDate = "25/08/1994";
+        assert.deepEqual(index._test.wide(strDate), "25 de agosto de 1994");
+    });
+  });
+
+  describe('wide', function () {
+    it('Wide: data vazia (deve retornar uma string vazia)', function () {
+        var strDate = "";
+        assert.deepEqual(index._test.wide(strDate), "");
+    });
+  });
+
+  describe('wide', function () {
+    it('Wide: formato errado 25-08-1994 (deve retornar uma string vazia)', function () {
+        var strDate = "06-10-1997";
+        assert.deepEqual(index._test.wide(strDate), "");
+    });
+  });
+
+});
