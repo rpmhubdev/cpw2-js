2da39717ce2d8f0f3e3c8f9e3025d9863d029ef4
Rafael Elias Ioppi <141973836+RafaelEliasIoppi@users.noreply.github.com>
2023-10-15 13:16:09 +0000

    Rafael

--- index.js
+++ index.js
@@ -6,39 +6,15 @@
  * @returns Um array com os nomes invertidos
  */
 function invert(people){
-
-   // Crie um novo vetor invertido
-  var invertedNames = [];
-  
-  // Itere pelo vetor de pessoas em ordem reversa e adicione os nomes ao vetor invertido
-  for (var i = people.length - 1; i >= 0; i--) {
-    invertedNames.push(people[i]);
-  }
-
-  // Retorne o vetor invertido
-  return invertedNames;
+ // Use o método reverse() para inverter o vetor
+  return people.reverse();
 }
 
-// Solicitar ao usuário que insira os nomes e armazená-los em um vetor
-var people = [];
-var input;
-
-do {
-  input = prompt("Digite um nome (ou deixe em branco para encerrar):");
-  if (input) {
-    people.push(input);
-  }
-} while (input);
-
-// Chame a função invert para obter o vetor de nomes invertidos
-var invertedPeople = invert(people);
-
-// Exiba os nomes invertidos na tela
-console.log("Nomes invertidos:");
-console.log(invertedPeople);
-
-    
-
+// Exemplo de uso da função:
+var nomes = ["Rafael", "Bob", "Carol", "David", "Eve"];
+var nomesInvertidos = invert(nomes);
+console.log(nomesInvertidos); // Isso irá exibir os nomes invertidos no console
+   
 
 /**
  * Essa função recebe um array de notas e retorna uma média
@@ -48,12 +24,19 @@ console.log(invertedPeople);
  */
 function mean(grades){
 
-    // TODO
-    // 2) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
-    // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
-    // ou superior a 7.
+  if (grades.length === 0) {
+    return 0; // Retorna 0 se o vetor estiver vazio para evitar divisão por zero.
+  }
 
-    return 0;
+  var soma = 0;
+
+  for (var i = 0; i < grades.length; i++) {
+    soma += grades[i];
+  }
+
+  return soma / grades.length;
+
+  
 }
 
 /**
@@ -64,12 +47,25 @@ function mean(grades){
  */
 function isApproved(mean){
 
-    // TODO
-    // 2.1) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
-    // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
-    // ou superior a 7.
+  var nome = prompt("Digite o nome do aluno: ");
+  var nota1 = parseFloat(prompt("Digite a primeira nota: "));
+  var nota2 = parseFloat(prompt("Digite a segunda nota: "));
+  var nota3 = parseFloat(prompt("Digite a terceira nota: "));
 
-    return "";
+  // Crie um vetor com as notas
+  var notas = [nota1, nota2, nota3];
+
+  // Use a função mean para calcular a média
+  var media = mean(notas);
+
+  // Exibir o nome do aluno
+  console.log("Nome do aluno: " + nome);
+
+  if (media >= 7) {
+    return "Aprovado";
+  } else {
+    return "Reprovado";
+  }
 }
 
 /**
@@ -80,14 +76,36 @@ function isApproved(mean){
  */
 function wide(strDate){
 
-    // TODO
-    // 3) Faça um script que receba uma data no formato “dd/mm/aaaa” e escreva a data por extenso,
-    // por exemplo, de "03/03/2022" para "03 de março de 2022". Dica: use a função
-    // “split” de uma string que quebra a string em pedaços dado um separador como argumento da função.
-    // Nesse caso, o separador é a barra (/) da data.
+     // Divida a string da data usando o separador "/"
+    var dateParts = dateString.split("/");
+    
+    if (dateParts.length === 3) {
+      var day = dateParts[0];
+      var month = dateParts[1];
+      var year = dateParts[2];
+      
+      // Crie um array de nomes de meses
+      var months = ["janeiro", "fevereiro", "março", "abril", "maio", "junho",
+        "julho", "agosto", "setembro", "outubro", "novembro", "dezembro" ];
+      
+      // Converta o mês em seu nome por extenso
+      var monthName = months[parseInt(month) - 1];
+      
+      // Crie a data por extenso
+      var strDate = day + " de " + monthName + " de " + year;
+      
+      return strDate;
+    } else {
+      return "Data inválida. Use o formato dd/mm/aaaa.";
+    }
+  }
+  
+  // Exemplo de uso da função:
+  var data = "03/03/2022";
+  var dataPorExtenso = wide(data);
+  console.log(dataPorExtenso); // Deverá exibir "03 de março de 2022" no console
+  
 
-    return "";
-}
 
 // Trecho utilizado pelos testes
 exports._test = {
